0cbbdcbed5c432c49f061b994eed8540cc34a4f1
xblazegmd <santiagopalaciocolina7@gmail.com>
2025-12-28 14:23:09 -0500

    got somehwhat of a functionality

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import tkinter as tk
 from tkinter import ttk, messagebox
 
+import requests
+
+API = "http://www.boomlings.com/database/"
+SECRET = "Wmfd2893gb7"
+HEADERS = { "User-Agent": "" }
+
 class App(tk.Tk):
     def __init__(self) -> None:
         super().__init__()
@@ -35,7 +41,12 @@ class App(tk.Tk):
         self.levelID = tk.StringVar()
 
         ttk.Label(self, text="Level ID").pack(pady=(10, 0))
-        levelIDEntry = ttk.Entry(self, textvariable=self.levelID)
+        levelIDEntry = ttk.Entry(
+            self,
+            textvariable=self.levelID,
+            validate="key",
+            validatecommand=(self.register(self.validateLevelIDEntry), "%P")
+        )
         levelIDEntry.pack(pady=(0, 10))
 
         # Test Comment
@@ -49,14 +60,51 @@ class App(tk.Tk):
         # The Moment of Truth
         ttk.Button(self, text="Am I Comment Banned?", command=self.onBtClick).pack(pady=10)
     
-    def onCommentEntryWrite(self, *args):
+    def validateLevelIDEntry(self, P: str) -> None:
+        return P.isdigit() or P == ""
+    
+    def onCommentEntryWrite(self, *args) -> None:
         charLimit = 100
         txt = self.comment.get()
         if len(txt) > charLimit:
             self.comment.set(txt[:charLimit])
     
-    def onBtClick(self):
-        messagebox.showinfo("TODO", "TODO")
+    def onBtClick(self) -> None:
+        username = self.username.get()
+        password = self.password.get()
+        levelID = int(self.levelID.get())
+        comment = self.comment.get()
+
+        # Get accountID
+        accIDReqParams = {
+            "secret": SECRET,
+            "str": username
+        }
+
+        accIDReq = requests.post(API + "getGJUsers20.php", data=accIDReqParams, headers=HEADERS)
+
+        if not accIDReq.ok:
+            messagebox.showerror(title="Error", message=f"Could not find user data: {accIDReq.text}")
+            return
+        
+        if accIDReq.text == "-1":
+            messagebox.showerror(title="Error", message=f"User '{username}' was not found")
+            return
+        
+        res = accIDReq.text.split(":")
+        accID = int(res[3])
+        print(accID)
+
+        # Try and upload comment
+        uploadCommentReqParams = {
+            "accountID": accID,
+            "gjp": "",
+            "comment": "",
+            "levelID": levelID,
+            "percent": 0,
+            "chk": "",
+            "secret": SECRET
+        }
 
 def main() -> None:
     app = App()
