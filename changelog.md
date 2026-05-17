b1b32ffc7217ac228402906bc231438fff17c39e
xblazegmd <santiagopalaciocolina7@gmail.com>
2026-05-17 16:49:49 -0500

    add changelog.md file and update changelog

---
+++ changelog.md
@@ -0,0 +1,7 @@
+# v1.1.0
+- Fix permabans
+- Change how duration is displayed
+- Fix my mess of a code (seriously what was wrong with me did I vibecode this or smth 😭)
+
+# v1.0.0
+- Initial Release
---
+++ format_changelog.py
@@ -0,0 +1,12 @@
+result = ""
+titleCount = 0
+with open("changelog.md", "r") as f:
+    for line in f:
+        if line.startswith("#"):
+            titleCount += 1
+            if titleCount > 1:
+                break
+
+        result += line
+
+print(result.strip(), end="")
